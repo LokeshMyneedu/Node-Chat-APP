7e526044609f1b0743c8be48eddc6b02160ac675
Lokesh Myneedu <nmyneedu@avidxchange.com>
2018-03-08 13:44:16 -0500

    added new event and broadcat mechanism

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT||3000;
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('New User Connected');
+    
+    socket.on('createMessage',(message)=>{
+       console.log('Got new message from client ',message);
+       io.emit('newMessage',{
+           from:message.from,
+           text:message.text,
+           createdAt:new Date().getTime()
+       })
+    })
+    socket.on('disconnect',()=>{
+        console.log('Client disconnected');
+    });
+});
+server.listen(port,()=>{
     console.log(`app started on port ${port}`);
 })
